71ff9236b7595db447334b237ed32f9bbfff3d2f
AdmiralAsshat <asshatadmiral@gmail.com>
2013-12-16 22:33:43 -0500

    Second half of logpuzzle working. All exercises finished.

--- google_code_university/logpuzzle/logpuzzle.py
+++ google_code_university/logpuzzle/logpuzzle.py
@@ -18,7 +18,12 @@ Here's what a puzzle url looks like:
 10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 """
 
-
+def sort_url(url):
+  match = re.search(r'(\w+)-(\w+).(\w+)\.jpg', url)
+  if match:
+    return match.group(3)
+  else:  return url
+  
 def read_urls(filename):
   """Returns a list of the puzzle urls from the given log file,
   extracting the hostname from the filename itself.
@@ -35,7 +40,7 @@ def read_urls(filename):
       to_add = 'http://' + source_url + match.group(1)
       if to_add not in images:
         images.append(to_add)
-  return sorted(images)
+  return sorted(images, key = sort_url)
 
 def download_images(img_urls, dest_dir):
   """Given the urls already in the correct order, downloads

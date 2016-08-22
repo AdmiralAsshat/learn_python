472b4f6d31da1a534da38619896b124b3aeef7d2
AdmiralAsshat <AsshatAdmiral@gmail.com>
2016-08-22 18:12:07 -0400

    Exercise prompt: implement Russian Peasant Algorithm

---
+++ intermediate python/russian_peasant_algorithm.py
@@ -0,0 +1,12 @@
+##: The Russian Peasant's Algorithm
+##: Been around a ling time (Seventeenth Century B.C.)
+
+##: Multiply two numbers together.
+##: Requirements: multiply by 2, divide by 2, and add numbers
+
+##: AKA = Mediation and Duplation Method
+
+##:
+##: Inputs -> two numbers
+##: Output -> the solution to those two numbers
+##:				multiplied together using the Russian Peasant Algorithm

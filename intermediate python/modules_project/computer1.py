4653ac934677440a4f00d0276c79b060c53bab32
AdmiralAsshat <AsshatAdmiral@gmail.com>
2016-08-23 16:49:19 -0400

    Modules project: computer1.py solution

---
+++ intermediate python/modules_project/computer1.py
@@ -0,0 +1,68 @@
+import fake_database
+
+CACHE = {}
+
+def printName():
+    return str(__name__)
+
+def updateLastMultiplied(a,b,result):
+    key = 'lastFive'
+    lastFiveList = CACHE.get(key)
+    if lastFiveList:
+        if len(lastFiveList) >= 5:
+            ## The list already had five items in it
+            newList = lastFiveList[1:]
+            newList.append('{}x{}={}'.format(a,b,result))
+            CACHE[key] = newList
+        else:
+            ## The list had less than five items
+            lastFiveList.append('{}x{}={}'.format(a,b,result))
+            CACHE[key] = lastFiveList
+    else:
+        ## There was not a cache so create one
+        CACHE[key] = ['{}x{}={}'.format(a,b,result)]
+            
+
+def lastMultipliedHandler():
+    """
+    Write this function.
+    Inputs : None
+    Outputs: The last multiplied result
+    This is the last 5 multiplied questions/answers
+    """
+    key = 'lastFive'
+    if key in CACHE:
+        print "Last 5 = {}".format(CACHE[key])
+        print "-" * 8
+        print " "
+    else:
+        print "Russian not used before."
+        print "-" * 8
+        print " "
+
+
+def multiplyHandler(a, b):
+    """
+    Write this function.
+    Inputs : a, b representing Numbers as arguments from the request.
+    Outputs: The result of those two numbers being sent thru
+                The Russuan Peasant's Algorithm.
+    """
+    cacheKey = (a,b)
+    if cacheKey in CACHE:
+        print CACHE[cacheKey]
+    else:
+        result = fake_database.rpa(a,b)
+        updateLastMultiplied(a,b, result)
+        CACHE[cacheKey] = result
+        print 'Latest Result: {}'.format(result)
+        lastMultipliedHandler()
+
+if __name__ == '__main__':
+    multiplyHandler(2, 3)
+    multiplyHandler(7, 16)
+    multiplyHandler(10, 20)
+    multiplyHandler(33, 66)
+    multiplyHandler(9, 150)
+    multiplyHandler(5627, 9534)
+    multiplyHandler(9999, 9999)
---
+++ intermediate python/modules_project/fake_database.py
@@ -0,0 +1,46 @@
+##: The Russian Peasant's Algorithm
+##: Been around a ling time (Seventeenth Century B.C.)
+
+##: Multiply two numbers together.
+##: Requirements: multiply by 2, divide by 2, and add numbers
+
+##: AKA = Mediation and Duplation Method
+
+##:
+##: Inputs -> two numbers
+##: Output -> the solution to those two numbers
+##:				multiplied together using the Russian Peasant Algorithm
+
+## Example:
+##24 	*	 36		#EVEN: Disregard
+##12 	*	 72		#EVEN: Disregard
+##6 	*	144 	#EVEN: Disregard
+##3 	* 	288		#ODD: Add
+##1 	*	576 	#ODD: Add
+##--------------
+## 288 + 576 = 864
+
+import time
+
+def rpa(x, y):
+  total = 0
+  while(x > 0):
+  	if (x % 2 == 1):	#If left-side is odd,
+  	  total +=y			#keep right-side number
+  	x = x >> 1			#Bit-shift to divide by two
+  	y = y << 1			#Bit-shift to multiply by two
+  return total
+
+def test_rpa():
+	start_time = time.time()
+	print rpa(25, 48)		#1200
+	print "Russian Algorithm took %f seconds" % (time.time() - start_time)
+
+	assert rpa(25, 48) == 1200
+	assert rpa(135, 233) == 31455
+	assert rpa(666, 999) == 665334
+	assert rpa(255, 1000) == 255000
+	print "All tests of function rpa successful."
+
+if __name__ == '__main__':
+	test_rpa()

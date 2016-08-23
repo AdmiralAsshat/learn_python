38692c7b313f6b90fe3076bd3d290e1f9580b875
AdmiralAsshat <AsshatAdmiral@gmail.com>
2016-08-23 15:59:51 -0400

    Cleanup Directory. All old exercises in LPTHW

--- ex2.py
+++
@@ -1,5 +0,0 @@
-print "I could have code like this." # and the comment after is ignored
-
-# print "This won't run."
-print "This will run."
-
--- script.py
+++
@@ -1,6 +0,0 @@
-fhand = open('err.jb')
-for line in fhand:
-	line = line.rstrip()
-	if line.find('skipcodes') == -1:
-		continue
-	print line

1073cce271076b51179c5dd634e9cc3bf04c708a
AdmiralAsshat <AsshatAdmiral@gmail.com>
2016-08-27 23:06:10 -0400

    Exercise Prompt: Puzzle Solver

---
+++ intermediate python/valid_function.py
@@ -0,0 +1,21 @@
+
+# Functions we will create:
+
+#    - solve   --> Runs thru all possible combinations testing each for valid
+#    - fill_in --> Create a new formula replacing letters with numbers
+#    - valid   --> Tests our filled-in string
+
+import ??
+
+def valid(formula):
+    """
+    Formula is valid only if it has no leading zero on any of it's numbers
+    and the formula evaluates as True.
+    Returns True or False
+    """
+    try:
+        ## your code here
+    except ArithmeticError:
+        ## your code here
+    except:
+        ## your code here

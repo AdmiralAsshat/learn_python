017c05fbb89a4858edbf97cda169363b47f0169d
AdmiralAsshat <AsshatAdmiral@gmail.com>
2016-08-29 20:19:38 -0400

    Lambdas

---
+++ intermediate python/lambda_intro.py
@@ -0,0 +1,21 @@
+
+## Lambda
+
+## They are a shorthand to create anonymous functions;
+## the expression lambda arguments: expression yields a function object.
+## The unnamed object behaves like a function object.
+
+## SEND + MORE == MONEY
+
+## Normal Function (Statement)
+
+def pre_compile(S,E,N,D,M,O,R,Y):
+    return (D+10*N+100*E+1000*S)+(E+10*R+100*O+1000*M) \
+           ==(Y+10*E+100*N+1000*O+10000*M)
+
+## Eval does take a statement, it takes an expression.
+
+## In comes Lambda --> (Greek) Branch of Mathematics defining Function
+
+lambda S,E,N,D,M,O,R,Y:(D+10*N+100*E+1000*S)+(E+10*R+100*O+1000*M) \
+       ==(Y+10*E+100*N+1000*O+10000*M)

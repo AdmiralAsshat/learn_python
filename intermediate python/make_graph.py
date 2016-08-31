24e9f405a6b989c9344258b9717ef51f8d8f4439
AdmiralAsshat <AsshatAdmiral@gmail.com>
2016-08-31 19:59:25 -0400

    Pairwise Connections

--- intermediate python/make_graph.py
+++ intermediate python/make_graph.py
@@ -30,3 +30,42 @@ def make_link(Graph, node1, node2):
         Graph[node2] = {}
     (Graph[node2])[node1] = 1
     return Graph
+
+connected_letters = [('a','g'),('a','d'),('d','g'),('g','c'), \
+           ('b','f'),('f','e'),('e','h')]
+
+for (x,y) in connected_letters: make_link(GRAPH, x, y)
+
+#####################################################################
+
+def marked_node(G, node, marked):
+    marked[node] = True
+    total_marked = 1
+    for neighbour in G[node]:
+        if neighbour not in marked:
+            total_marked += marked_node(G, neighbour, marked)
+    return total_marked
+
+def list_node_sizes(G):
+    marked = {}
+    for node in G.keys():
+        if node not in marked:
+            print "Graph containing", node, \
+                  ":", marked_node(G, node, marked)
+
+#########################
+### Pairwise Connection
+#########################
+
+## Depth First Search Solution
+def is_connected(G, node1, node2):
+    marked = {}
+    marked_node(G, node1, marked)
+    return node2 in marked
+
+
+
+#list_node_sizes(GRAPH)
+print is_connected(GRAPH, 'a','h')
+
+            

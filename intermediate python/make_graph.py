8905c8667d78dd2f88c029bac988a151d1ea5cca
AdmiralAsshat <AsshatAdmiral@gmail.com>
2016-08-30 23:19:45 -0400

    Exercise: Solution

--- intermediate python/make_graph.py
+++ intermediate python/make_graph.py
@@ -23,5 +23,10 @@ def make_link(Graph, node1, node2):
     it's value will be 1.
     Return the updated Graph.
     """
-    ## your code here
-    pass
+    if node1 not in Graph:
+        Graph[node1] = {}
+    (Graph[node1])[node2] = 1
+    if node2 not in Graph:
+        Graph[node2] = {}
+    (Graph[node2])[node1] = 1
+    return Graph

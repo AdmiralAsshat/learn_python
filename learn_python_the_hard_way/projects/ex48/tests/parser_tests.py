27e426e0d41552fa8c4b4ebcbca03c265a07f8a8
AdmiralAsshat <AsshatAdmiral@gmail.com>
2016-09-09 22:15:06 -0400

    Exercise 49: Create parser tests

---
+++ learn_python_the_hard_way/projects/ex48/tests/parser_tests.py
@@ -0,0 +1,18 @@
+from nose.tools import *
+from ex48 import parser
+
+## Test every function in parser
+
+def test_peek:
+	pass
+
+
+def test_match:
+	pass
+
+def test_match:
+	pass
+
+def test_skip:
+	pass
+

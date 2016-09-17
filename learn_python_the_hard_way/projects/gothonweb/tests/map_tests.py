4e44a1652d546734fd80679dd87e682eaaab9050
AdmiralAsshat <AsshatAdmiral@gmail.com>
2016-09-17 14:59:15 -0400

    Nosetests pass. Now fill out the rest.

--- learn_python_the_hard_way/projects/gothonweb/tests/map_tests.py
+++ learn_python_the_hard_way/projects/gothonweb/tests/map_tests.py
@@ -1,5 +1,5 @@
 from nose.tools import *
-from gothonweb.map import Room
+from gothonweb.map import *
 
 def test_room():
 	gold = Room("GoldRoom",
@@ -29,3 +29,10 @@ def test_map():
 	assert_equal(start.go('west'), west)
 	assert_equal(start.go('west').go('east'), start)
 	assert_equal(start.go('down').go('up'), start)
+
+def test_gothon_game_map():
+	assert_equal(START.go('shoot!'), generic_death)
+	assert_equal(START.go('dodge!'), generic_death)
+
+	room = START.go('tell a joke')
+	assert_equal(room, laser_weapon_armory)

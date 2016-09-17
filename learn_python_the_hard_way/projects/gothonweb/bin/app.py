052dfed8aa1862eb35cfe0980a3e534b44be5b13
AdmiralAsshat <AsshatAdmiral@gmail.com>
2016-09-16 23:22:03 -0400

    Page no longer loads.

--- learn_python_the_hard_way/projects/gothonweb/bin/app.py
+++ learn_python_the_hard_way/projects/gothonweb/bin/app.py
@@ -1,16 +1,20 @@
 import web
 
 urls = (
-	'/', 'index'
+	'/hello', 'Index'
 )
 
 app = web.application(urls, globals())
 
-render = web.template.render('templates/')
+render = web.template.render('templates/', base="layout")
 
-class index:
+class Index:
 	def GET(self):
-		greeting = "Hello World"
+		return render.hello_form()
+
+	def POST(self):
+		form = web.input(name="Nobody", greet="Hello")
+		greeting = "%s, %s" % (form.greet, form.name)
 		return render.index(greeting = greeting)
 
 if __name__ == '__main__':

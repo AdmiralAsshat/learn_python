4556bfbb18fb8028d091f81a05c600f0f33eb685
AdmiralAsshat <AsshatAdmiral@gmail.com>
2016-09-18 21:17:56 -0400

    TODO: App fails on browser load. Get working.

--- learn_python_the_hard_way/projects/gothonweb/bin/app.py
+++ learn_python_the_hard_way/projects/gothonweb/bin/app.py
@@ -1,21 +1,49 @@
 import web
+from gothonweb import map
 
 urls = (
-	'/hello', 'Index'
+	'/game', 'GameEngine'
+	'/', 'Index',
 )
 
 app = web.application(urls, globals())
 
+# little hack so that debug mode works with sessions
+if web.config.get('_session') is None:
+	store = web.session.DiskStore('sessions')
+	session = web.session.Session(app, store,
+									initializer={'room': None})
+	web.config._session = session
+else:
+	session = web.config._session
+
 render = web.template.render('templates/', base="layout")
+
 
 class Index(object):
 	def GET(self):
-		return render.hello_form()
+		# this is used to "setup" the session with starting values
+		session.room = map.START
+		web.seeother("/game")
+
+
+class GameEngine(object):
+
+	def GET(self):
+		if session.room:
+			return render.show_room(room=session.room)
+		else:
+			# why is this here? Do you need it?
+			return render.you_died()
 
 	def POST(self):
-		form = web.input(name="Nobody", greet="Hello")
-		greeting = "%s, %s" % (form.greet, form.name)
-		return render.index(greeting = greeting)
+		form = web.input(action=None)
+
+		# there is a bug here, can you fix it?
+		if session.room and form.action:
+			session.room = session.room.go(form.action)
+
+		web.seeother("/game")
 
 if __name__ == "__main__":
 	app.run()

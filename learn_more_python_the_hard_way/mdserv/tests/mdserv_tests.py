c4f3f2baec61f12f1b7f3a8e68917330f42b7987
AdmiralAsshat <AsshatAdmiral@gmail.com>
2016-10-01 21:30:37 -0400

    Leave me with a broken app, will you? Dammit, Zed.

--- learn_more_python_the_hard_way/mdserv/tests/mdserv_tests.py
+++ learn_more_python_the_hard_way/mdserv/tests/mdserv_tests.py
@@ -16,5 +16,16 @@ class FlaskrTestCase(unittest.TestCase):
         page = self.app.get('/foo.html')
         assert_true('snap' in page.data)
 
+    def test_edit(self):
+        sample = open('docs/foo.html').read()
+        page = self.app.get('/edit/foo.html')
+        assert_true('textarea', page.data)
+
+        page = self.app.post(
+            '/edit/foo.html', 
+            data={'contents': sample},
+             follow_redirects=True)
+        assert_false('textarea', page.data)
+
 if __name__ == '__main__':
     unittest.main()

1c3c028e74eb311c13d87d8123bfef4f41e8f79c
AdmiralAsshat <AsshatAdmiral@gmail.com>
2016-10-01 21:11:32 -0400

    Move stuff out of mdserve, into blog.py

--- learn_more_python_the_hard_way/mdserv/tests/mdserv_tests.py
+++ learn_more_python_the_hard_way/mdserv/tests/mdserv_tests.py
@@ -1,11 +1,20 @@
 from nose.tools import *
-import mdserv
+from mdserv import blog
+import unittest
 
-def setup():
-	print "SETUP!"
+class FlaskrTestCase(unittest.TestCase):
 
-def teardown():
-	print "TEAR DOWN!"
+    def setUp(self):
+        blog.app.config['TESTING'] = True
+        self.app = blog.app.test_client()
 
-def test_basic():
-	print "I RAN!"
+    def test_index(self):
+        page = self.app.get('/')
+        assert_true('Welcome to LAME BLOG' in page.data)
+
+    def test_mdfile(self):
+        page = self.app.get('/foo.html')
+        assert_true('snap' in page.data)
+
+if __name__ == '__main__':
+    unittest.main()

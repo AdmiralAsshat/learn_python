8d8b6db696078bf289324de70e2f7488edb7f92e
AdmiralAsshat <AsshatAdmiral@gmail.com>
2016-10-27 21:52:15 -0400

    Add try/except to collatz

--- automate_the_boring_stuff/collatz.py
+++ automate_the_boring_stuff/collatz.py
@@ -1,19 +1,31 @@
+#!/usr/bin/env python3
+
 # The Collatz Sequence
 # Take a number and run collatz function until result is one
 # If even, floor divide by two
 # If odd, multiply by three and add one
 def collatz(num):
     number = int(num)
+
     if (number % 2 == 0):
         return (number // 2)
     else:
         return ((number * 3) + 1)
 
 def main():
-    num = input("Please enter a number: ")
-    while (int(num) > 1):
-        num = collatz(num)
-        print(num)
+    try:
+        num = input("Please enter a number: ")
+    except NameError:
+        print("You must enter an integer.")
+        return -1
+
+    try:
+        while (int(num) > 1):
+            num = collatz(num)
+            print(num)
+    except ValueError:
+        print("You must enter an integer.")
+        return -1
 
 if __name__ == '__main__':
     main()
